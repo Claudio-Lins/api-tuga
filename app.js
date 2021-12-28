251018eb6433cac88bf5abd4997678982d92b069
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2021-12-28 20:28:30 +0000

    Update app.js

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const cors = require('cors');
 const app = express();
 require('./Routes/index')(app);
 
-dotenv.config();
 
 app.use(cors());
 app.use(express.json());
 
 
-app.listen(3333);
+app.listen(3333, () => {
+    console.log('Server started on port 3333!');
+});
