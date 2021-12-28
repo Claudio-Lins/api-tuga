4ddcc594241c0a5d06e47ea977eb85416ef4740a
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2021-12-28 20:31:51 +0000

    Add dotenv

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express');
+import dotenv from 'dotenv';
 
 const cors = require('cors');
 const app = express();
 require('./Routes/index')(app);
 
+dotenv.config();
 
 app.use(cors());
 app.use(express.json());
 
 
-app.listen(3333, () => {
-    console.log('Server started on port 3333!');
+app.listen(process.env.PORT, () => {
+    console.log('Server started on port 4000!');
 });
