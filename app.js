9054515f84c4430eee42c65edce4d05d7812308d
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2021-12-28 20:15:08 +0000

    Server run on PORT 4000

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require('express');
+import dotenv from 'dotenv';
+
 const cors = require('cors');
 const app = express();
 require('./Routes/index')(app);
+
+dotenv.config();
+
 app.use(cors());
 app.use(express.json());
-app.listen(3333);
+
+
+
+// app.listen(3333);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
