66467b40c212388419343ae0e66f1cddb1b05357
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2021-12-28 20:21:52 +0000

    Update app.js

--- app.js
+++ app.js
@@ -10,13 +10,6 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 
-app.use((req, res) => {
-  res.status(404)
-  res.json({ error: 'Page not found!' });
-})
 
-// app.listen(3333);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
-});
+app.listen(3333);
