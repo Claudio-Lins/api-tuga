a283ee837e134f9b185b6e8062e05bf3914dbc62
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2021-12-30 17:57:59 +0000

    Newsletter Routes crested

--- app.js
+++ app.js
@@ -11,56 +11,8 @@ const Newsletter = require('./models/Newsletter');
 app.use(express.json());
 
 //ROUTES 
-app.use('/newsletter', newsletter);
-
-app.get('/newsletter/:id', (req, res) => {
-  Newsletter.findByPk(req.params.id)
-    .then(newsletter => {
-      res.json(newsletter);
-    })
-    .catch(err => {
-      res.json({ error: err });
-    });
-})
-
-app.post('/newsletter', (req, res) => {
-  Newsletter.create(
-    req.body
-  ).then(function() {
-    res.send('Newsletter created');
-  }).catch(function(err) {
-    res.send(err);
-  })
-});
-
-app.put('/newsletter/:id', (req, res) => {
-  Newsletter.update(
-    req.body,
-    {
-      where: {
-        id: req.params.id
-      }
-    }
-  ).then(function() {
-    res.send('Newsletter updated');
-  }).catch(function(err) {
-    res.send(err);
-  })
-})
-
-app.delete('/newsletter/:id', (req, res) => {
-  Newsletter.destroy(
-    {
-      where: {
-        id: req.params.id
-      }
-    }
-  ).then(function() {
-    res.send('Newsletter deleted');
-  }).catch(function(err) {
-    res.send(err);
-  })
-})
+app.use('/', newsletter);
+app.use('/:id', newsletter); 
 
 app.listen(4000, () => {
   console.log('Listening on port 4000');
--- routes/newsletter.js
+++ routes/newsletter.js
@@ -1,12 +1,65 @@
 const express = require('express');
 const router = express.Router();
 
+const Newsletter = require('../models/Newsletter');
+
+
 
 router.get('/newsletter', (req, res) => {
     Newsletter.findAll()
       .then(newsletter => res.json(newsletter))
       .catch(error => res.status(400).json(error));
   });
+
+router.get('/newsletter/:id', (req, res) => {
+  Newsletter.findByPk(req.params.id)
+    .then(newsletter => {
+      res.json(newsletter);
+    })
+    .catch(err => {
+      res.json({ error: err });
+    });
+})
+
+router.post('/newsletter', (req, res) => {
+  Newsletter.create(
+    req.body
+  ).then(function() {
+    res.send('Newsletter created');
+  }).catch(function(err) {
+    res.send(err);
+  })
+});
+
+
+router.put('/newsletter/:id', (req, res) => {
+  Newsletter.update(
+    req.body,
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  ).then(function() {
+    res.send('Newsletter updated');
+  }).catch(function(err) {
+    res.send(err);
+  })
+})
+
+router.delete('/newsletter/:id', (req, res) => {
+  Newsletter.destroy(
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  ).then(function() {
+    res.send('Newsletter deleted');
+  }).catch(function(err) {
+    res.send(err);
+  })
+})
 
 
 
