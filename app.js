fc8058151eff8007621ec3d1b23fefd03610ce69
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2021-12-30 13:20:12 +0000

    Update app.js
    salvando no DB

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require('express');
+const app = express();
+
+app.use(express.json());
 
 const Newsletter = require('./models/Newsletter');
-
-const app = express();
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World');
+});
+
+app.post('/newsletter', async (req, res) => {
+  const createNewsletter = await Newsletter.create(
+    req.body
+  ).then(function() {
+    res.send('Newsletter created');
+  }).catch(function(err) {
+    res.send(err);
+  })
 });
 
 app.listen(4000, () => {
