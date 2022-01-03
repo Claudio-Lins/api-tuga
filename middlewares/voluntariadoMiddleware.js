57e17fb1de92e9f1169e82d1efce9245bf246684
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2022-01-03 20:24:01 +0000

    To public/voluntariado

--- middlewares/voluntariadoMiddleware.js
+++ middlewares/voluntariadoMiddleware.js
@@ -4,7 +4,7 @@ const path = require("path");
 module.exports = multer({
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, "./tmp");
+      cb(null, "./public/voluntariado");
     },
     filename: (req, file, cb) => {
       cb(null, Date.now().toString() + "_" + file.originalname);
