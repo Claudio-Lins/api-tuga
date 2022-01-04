2d085b41729e9d2dd17ffa7330bb7d7b29ea604e
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2022-01-04 19:27:46 +0000

    route imprensa

--- routes/imprensa.js
+++ routes/imprensa.js
@@ -1,73 +1,72 @@
-const express = require("express");
-const router = express.Router();
-const sharp = require("sharp");
+const express = require('express')
+const router = express.Router()
+const sharp = require('sharp')
 // const unlink = require("fs/promises");
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs')
+const path = require('path')
 
-const uploadFile = require("../middlewares/imprensaMiddleware");
+const uploadFile = require('../middlewares/imprensaMiddleware')
 
-const Imprensa = require("../models/Imprensa");
+const Imprensa = require('../models/Imprensa')
 
-router.get("/imprensa", (req, res) => {
-  Imprensa
-    .findAll()
+router.get('/imprensa', (req, res) => {
+  Imprensa.findAll()
     .then((imprensa) => res.json(imprensa))
-    .catch((error) => res.status(400).json(error));
-});
+    .catch((error) => res.status(400).json(error))
+})
 
-router.get("/imprensa/:id", (req, res) => {
+router.get('/imprensa/:id', (req, res) => {
   Imprensa.findByPk(req.params.id)
     .then((imprensa) => {
-      res.json(imprensa);
+      res.json(imprensa)
     })
     .catch((err) => {
-      res.json({ error: err });
-    });
-});
+      res.json({ error: err })
+    })
+})
 
-router.post("/imprensa", uploadFile.single("destaque"), async (req, res) => {
-  if (req.file.mimetype == "application/pdf") {
-    const { filename } = req.file;
-    const { title, subtitle } = req.body;
+router.post('/imprensa', uploadFile.single('destaque'), async (req, res) => {
+  if (req.file.mimetype == 'application/pdf') {
+    const { filename } = req.file
+    const { title, subtitle } = req.body
     const newImprensa = await Imprensa.create({
       title,
       subtitle,
       fileUrl: filename,
-    });
-    res.json({ newImprensa });
+    })
+    res.json({ newImprensa })
   } else {
-    res.status(400);
-    res.json({ error: "Somente aceito .PDF" });
+    res.status(400)
+    res.json({ error: 'Somente aceito .PDF' })
   }
-});
+})
 
-router.put("/imprensa/:id", (req, res) => {
+router.put('/imprensa/:id', (req, res) => {
   Imprensa.update(req.body, {
     where: {
       id: req.params.id,
     },
   })
     .then(function () {
-      res.send("imprensa updated");
+      res.send('imprensa updated')
     })
     .catch(function (err) {
-      res.send(err);
-    });
-});
+      res.send(err)
+    })
+})
 
-router.delete("/imprensa/:id", (req, res) => {
+router.delete('/imprensa/:id', (req, res) => {
   Imprensa.destroy({
     where: {
       id: req.params.id,
     },
   })
     .then(function () {
-      res.send("imprensa deleted");
+      res.send('imprensa deleted')
     })
     .catch(function (err) {
-      res.send(err);
-    });
-});
+      res.send(err)
+    })
+})
 
-module.exports = router;
+module.exports = router
